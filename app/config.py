cb02d63667d2b92a09725a5c83298fc976b5cd50
Teruteru21623 <teruteru.code623@gmail.com>
2025-06-19 23:33:56 +0900

    DATABASE

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ class Config:
     if url and url.startswith("postgres://"):
         url = url.replace("postgres://", "postgresql://", 1)
     
-    SQLALCHEMY_DATABASE_URI = url #or "postgresql://reagent_user:Tenor623@localhost:5432/reagent_search_dev"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:jYGbtOurqvVenPdQNCjOzccImjXOgBVs@postgres.railway.internal:5432/railway" #or "postgresql://reagent_user:Tenor623@localhost:5432/reagent_search_dev"
 
     # SQLAlchemyの変更通知機能を無効化（不要な警告を防ぐ）
     SQLALCHEMY_TRACK_MODIFICATIONS = False
